c7323daf80ce9c2a1475960c911d19f48b3bd6bb
Rodrigo Valle <rdan.valle@gmail.com>
2017-04-21 16:01:57 -0700

    Implemented move and write operations on a File

--- file.cpp
+++ file.cpp
@@ -7,7 +7,16 @@
 
 
 File::File(int fd) : fd(fd) {}
+File::File(File&& other) {
+  fd = other.fd;
+  other.fd = -1;
+}
 File::~File() { close(fd); }
+
+File& File::operator=(File&& other) {
+  fd = other.fd;
+  other.fd = -1;
+}
 
 File File::open(std::string& file, int flags) {
   int fd;
@@ -16,6 +25,28 @@ File File::open(std::string& file, int flags) {
     throw std::runtime_error("open(): " + std::string(strerror(errno)));
   }
   return FileDescriptor{fd};
+}
+
+ssize_t write(const char *buf, size_t nbytes) {
+  int r;
+  if ((r = ::write(fd, buf, nbytes)) == -1) {
+    throw std::runtime_error("write(): " + std::string(strerror(errno)));
+  }
+  return r;
+}
+
+void write_all(const char *buf, size_t nbytes) {
+  ssize_t total = 0;
+  ssize_t left = nbytes;
+  ssize_t n;
+
+  while (total_written < nbytes) {
+    if ((n = write(fd, buf + total_written, left)) == -1) {
+      throw std::runtime_error("write(): " + std::string(strerror(errno)));
+    }
+    left -= n;
+    total += n;
+  }
 }
 
 File File::open_r(std::string& file) {
--- file.hpp
+++ file.hpp
@@ -8,6 +8,8 @@ class File {
   File& operator=(const File&) = delete;
   File& operator=(File&&);
 
+  ssize_t write(char *buf, size_t nbyte);
+  void write_all(char *buf, size_t nbyte);
   static File open_r(std::string& file);
   static File create_w(std::string& file);
 
